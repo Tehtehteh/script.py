3bf3c9b0c68dc7d713a675149ccfad0ce13f6c5d
Tehtehteh <tehfvpr@gmail.com>
2016-08-09 14:56:50 +0300

    Polishing.

--- Monitor/homepage/views.py
+++ Monitor/homepage/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from .models import File, Users
-# Create your views here.
 
 
 def index(request):
     file_list = File.objects.all()
-    print(file_list[0].path)
-    return render(request, "index.html", {'objects':file_list})
+    user_list = Users.objects.all()
+    return render(request, "index.html", {'objects':file_list, 'users':user_list})
