75cde9c0b52934e62e442ab362d591b2ecb5d4e4
Tehtehteh <tehfvpr@gmail.com>
2016-08-09 15:26:06 +0300

    Polishing.

--- Monitor/homepage/views.py
+++ Monitor/homepage/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from .models import File, Users
+from django.contrib.auth.decorators import login_required
 
-
+@login_required(login_url='/admin/')
 def index(request):
     file_list = File.objects.all()
     user_list = Users.objects.all()
     return render(request, "index.html", {'objects':file_list, 'users':user_list})
+
+
+
+
+@login_required(login_url="/admin/")
+def user(request, idn=None):
+    User = Users.objects.filter(name=idn)
+    file_list = File.objects.filter(name=User)
+    return render(request, "user.html", {'file_list':file_list})
