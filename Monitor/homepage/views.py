c719c097c47f657d492ec8bb42687e7761a68dba
Tehtehteh <tehfvpr@gmail.com>
2016-08-11 13:10:08 +0300

    Changed a bit of logic.

--- Monitor/homepage/views.py
+++ Monitor/homepage/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render
 from .models import File, Users
 from django.contrib.auth.decorators import login_required
-from django.db.models import F, Q
+from configparser import ConfigParser
 
 @login_required(login_url='/admin/')
 def index(request):
-    file_list = File.objects.all()
     user_list = Users.objects.all()
-    return render(request, "index.html", {'objects':file_list, 'users':user_list})
+    return render(request, "index.html", {'users':user_list})
 
 
 
 #todo add state flag to each users and count in reviews.
 
-@login_required(login_url="/admin/")
+@login_required(login_url='/admin/')
 def user(request, idn=None):
-    User = Users.objects.filter(name=idn)
-    file_list = File.objects.filter(name=User)
-    new_file_list = File.objects.filter(old_hash='').all()
-    return render(request, 'user.html', {'file_list':file_list})
+    file_list = File.objects.filter(name=idn)
+    php_len = len(list(filter(lambda x: x.path.endswith(".php"),list(file_list))))
+    ext = {}
+    return render(request, 'user.html', {'file_list':file_list, 'php': php_len, 'js':len(list(filter(lambda x: x.path.endswith(".js"), file_list)))})
