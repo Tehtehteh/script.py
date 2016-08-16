67c9b3f87617471411878cb98f0a364e84f0ef9c
Tehtehteh <tehfvpr@gmail.com>
2016-08-16 11:30:06 +0300

    Minor fix regarding braces.

--- Monitor/homepage/static/js/my.js
+++ Monitor/homepage/static/js/my.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     loadFiles(window.location.pathname.slice(1,window.location.pathname.length));
 });
 var flags = {'New':"label-info", 'Changed':'label-warning', 'Checked':'label-success', 'Removed':'label-danger'};
+
 function loadFiles(username){
         console.log("working on files @", username);
         $.ajax({
@@ -11,8 +12,10 @@ function loadFiles(username){
             success: function(files){
             console.log(files[0].path.split(".")[files[0].path.split(".").length-1]);
                 for (var i =0; i < files.length; i++){
-                    $(".my-tbody").append("<tr>\n<th scope='row'>"+i+"</th>\n<td><span class=" + ("'label " + flags[files[i].flag]) +  "'>" + files[i].flag +"</span></td>" +  "<td>" + files[i].path +
-                   "</td>\n<td>" + files[i].path.split(".")[files[i].path.split(".").length-1] + "</td>")
+                    var str = "<tr>\n<th scope='row'>"+i+"</th>\n<td><span class=" + ('"label ' + flags[files[i].flag]) +  '">' + files[i].flag +"</span></td>" +  "<td>" + files[i].path +
+                   "</td>\n<td>" + files[i].path.split(".")[files[i].path.split(".").length-1] + "</td>";
+                console.log(str);
+                    $(".my-tbody").append(str);
 
                 }}
             });
