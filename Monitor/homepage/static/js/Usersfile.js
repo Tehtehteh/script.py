4b84d7fb7af6cd193432734fb96f850f1a1597ea
MrP4p3r <mrp4p3r@gmail.com>
2016-08-18 16:12:36 +0300

    Плавная анимация аккордеонов

--- Monitor/homepage/static/js/Usersfile.js
+++ Monitor/homepage/static/js/Usersfile.js
@@ -9,7 +9,7 @@ var i;
 for (i = 0; i < acc.length; i++) {
     acc[i].onclick = function(){
         this.classList.toggle("active");
-        this.nextElementSibling.classList.toggle("show");
+        $(this.nextElementSibling).slideToggle('fast'); /* 0.2s transition */
     }
 }
 });
