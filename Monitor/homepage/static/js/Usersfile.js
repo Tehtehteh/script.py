5a9a707def0b2ea586772dd263e6c44189dc55c8
Tehtehteh <tehfvpr@gmail.com>
2016-08-18 10:16:04 +0300

    Ajax loading of flag files count.

--- Monitor/homepage/static/js/Usersfile.js
+++ Monitor/homepage/static/js/Usersfile.js
@@ -2,21 +2,36 @@
 $(document).ready(function() {
     loadFiles(window.location.toString().split("/")[window.location.toString().split("/").length-1]);
     loadFilesCount(window.location.toString().split("/")[window.location.toString().split("/").length-1]);
+    loadFlagFilesCount(window.location.toString().split("/")[window.location.toString().split("/").length-1]);
     var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
     acc[i].onclick = function(){
-        console.log("added onclick at button", acc[i]);
         this.classList.toggle("active");
         this.nextElementSibling.classList.toggle("show");
     }
 }
 });
 var flags = {'New':"label-info", 'Changed':'label-warning', 'Checked':'label-success', 'Removed':'label-danger'};
+flagcount = {"Changed" : 0, "Removed": 0, "New":0, "Checked" : 0}
+
+function loadFlagFilesCount(username){
+    $.ajax({
+        url: "/api/flagfiles/" + username,
+        type: "GET",
+        success: function(files){
+            for (let i = 0; i < files.length; i ++){
+            flagcount[files[i].flag] ++ ;}
+            console.log(flagcount);
+            for (x in flagcount){
+                $(".badge-"+x).append(flagcount[x]);}
+        }
+    })
+
+}
 
 function loadFiles(username){
-        console.log("working on files @", username);
         $.ajax({
             url: "/api/flagfiles/" + username,
             type: "GET",
